6c963d6b9842daebef173ee7e3cc44eb6f168a30
Julia Fong <juliafong@Julias-MacBook-Pro.local>
2020-01-10 17:38:41 -0600

    Adds all of chapter one

--- js/app.js
+++ js/app.js
@@ -47,3 +47,79 @@ console.log(2 < 1)
 
 console.log("Camel" < "Zebra")
 //true "the way strings are ordered is more or less alphabetic"
+
+console.log("Stinky" != "Shower")
+//true
+
+console.log(NaN === NaN)
+//false Nan is supposed to denote the result of a nonsensical computation,
+//and as such, it isn't equal to the result of any other nonsensical
+//computations
+
+// LOGICAL OPERATORS
+//Not is written as an exclamation mark(!)
+
+ console.log(true || false)
+ //true
+
+ console.log(false || false)
+ //false
+
+ console.log(false || true)
+ //true
+
+ console.log(true || true)
+ //true
+
+ console.log(false && false)
+ //false
+
+ console.log(true && false || true)
+ //true
+
+//terenary, operating on three values, written with a question mark
+//and a colon 
+
+console.log(false ? 2 : 6)
+//6
+
+console.log(true ? 6 : 66)
+//6
+
+console.log(false ? -77 : 77)
+//77
+
+//^^ called the conditional operator or
+//just a ternary operator
+//value on the left of the question mark "picks" which other two values
+//will come out when it is true, the middle value is chosen, 
+//&& when it's false, the value on the right comes out
+
+//UNDEFINED VALUES
+
+console.log(10 * 9 * null)
+//0
+
+console.log("-77" + 22)
+//-7722
+
+console.log("55" - 1)
+//54
+
+console.log("seven" % 3)
+//NaN
+
+console.log(false === 0)
+//false
+
+console.log(true === -55)
+//false
+
+console.log(null || "julia")
+//julia
+
+console.log("Julia" || "user")
+//Julia
+
+console.log("Julia" || user || jules)
+//Julia
